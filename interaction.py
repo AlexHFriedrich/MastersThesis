2984cf21be7778fdad8fa2534f1660af9b3b8908
alexh <alex.h.friedrich@gmail.com>
2024-08-07 16:09:26 +0200

    both actors

--- interaction.py
+++ interaction.py
@@ -5,21 +5,25 @@ def main():
     session_state = [
         {"role": "system", "content": "You are convinced that talking with a full mouth is the worst offense possible"}]
 
-    while True:
-        user_input = input("You: ")
-        if user_input == "q":
-            break
-
-        session_state.append({"role": "user", "content": user_input})
-        response = ollama.chat(
+    session_state_second_instance = [{"role": "system", "content": "You really like to talk while eating"}]
+    while len(session_state) < 10:
+        first_actor = ollama.chat(
+            model="llama3.1",
+            messages=session_state_second_instance,
+            stream=False
+        )
+        print("First:", first_actor['message']['content'])
+        session_state.append({"role": "user", "content": first_actor['message']['content']})
+        second_actor = ollama.chat(
             model="llama3.1",
             messages=session_state,
             stream=False
         )
-        print("Bot:", response['message']['content'])
+        print("Second:", second_actor['message']['content'])
 
-        session_state.append({"role": "assistant", "content": response['message']['content']})
-        print(session_state)
+        session_state.append({"role": "assistant", "content": second_actor['message']['content']})
+
+    print(session_state)
 
 
 if __name__ == "__main__":
