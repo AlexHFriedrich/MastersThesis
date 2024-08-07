6d0e2cc54899856070efdd23b64bf4c6630dcd67
alexh <alex.h.friedrich@gmail.com>
2024-08-07 17:02:31 +0200

    both actors

--- interaction.py
+++ interaction.py
@@ -11,8 +11,8 @@ def main():
         {"role": "system", "content": "You really like to talk while eating and like talking about it even more"}]
     run_times = []
 
-    while len(session_state) < 30:
-        start = time.time()
+    while len(session_state) < 10:
+
         first_actor = ollama.chat(
             model="llama3.1",
             messages=session_state_second_instance,
@@ -21,6 +21,9 @@ def main():
         # print("First:", first_actor['message']['content'], "\n")
         session_state.append({"role": "user", "content": first_actor['message']['content']})
         session_state_second_instance.append({"role": "user", "content": first_actor['message']['content']})
+
+        # measure the time it takes to sample and choose a response
+        start = time.time()
         possible_answers = []
         while len(possible_answers) < 5:
             possible_answers.append(ollama.chat(
@@ -32,6 +35,8 @@ def main():
         for item in possible_answers:
             print(item)
         choice = 1
+        # here would be the RL agent choosing an optimal solution out of the sampled ones
+
         second_actor = possible_answers[choice]
         # print("Second:", second_actor['message']['content'], "\n")
 
