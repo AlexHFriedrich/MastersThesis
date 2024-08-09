afd1caa91992d240e93533d7bdbc489c288ec99d
alexh <alex.h.friedrich@gmail.com>
2024-08-09 14:25:22 +0200

    testing the fine-tuned model on ollama outputs

--- Testing_SA_on_Ollama.py
+++ Testing_SA_on_Ollama.py
@@ -11,7 +11,7 @@ def main():
     session_state_second_instance = [
         {"role": "system", "content": "You really like to talk while eating and like talking about it even more"}]
 
-    # model, tokenizer = load_model("ft_bert_emotion")
+    model, tokenizer = load_model("ft_bert_emotion")
 
     while len(session_state) < 10:
         start = time.time()
@@ -21,11 +21,11 @@ def main():
             stream=False,
             options={"num_predict": 50}
         )
-        '''if len(first_actor['message']['content']) > 0:
+        if len(first_actor['message']['content']) > 0:
             print("First:", first_actor['message']['content'], "\n")
             # here the SA model would be called to determine the sentiment of the first actor output
             sentiment = call_model(model, tokenizer, first_actor['message']['content'])
-            print("Sentiment Value:", sentiment)'''
+            print("Sentiment Value:", sentiment)
         # print("First:", first_actor['message']['content'], "\n")
         session_state.append({"role": "user", "content": first_actor['message']['content']})
         session_state_second_instance.append({"role": "user", "content": first_actor['message']['content']})
